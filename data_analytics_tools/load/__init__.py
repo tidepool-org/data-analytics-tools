bd8616f45680579753f1476c27f68ba1984558bd
rpwils <rpwils@monsanto.com>
2019-01-18 19:26:24 -0600

    initial

---
+++ data_analytics_tools/load/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+description: initialize the tidepool data analytics tools (tidals) python pacakge
+version: 0.0.1
+created: 2018-07-21
+author: Ed Nykaza
+license: BSD-2-Clause
+"""
