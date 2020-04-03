6a82087a8c8d218ba9da165c5c64fbfcedb5fe68
russ <russ@tidepool.org>
2020-04-02 19:07:31 -0500

    cleaned up code

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -1,7 +1,8 @@
 import numpy as np
+from typing import Tuple
 
 
-def cv_of_glucose(values):
+def cv_of_glucose(bg_values):
     """
         Calculate the average within a set of glucose values
 
@@ -10,15 +11,34 @@ def cv_of_glucose(values):
 
         Output: Calculated Average
     """
-    std_dev = std_deviation(values)
-    avg_glu = avg_glucose(values)
+    std_dev = std_deviation(bg_values)
+    avg_glu = avg_glucose(bg_values)
     return round(std_dev/avg_glu * 100, 2)
 
-#For example, if the SD is 50 mg/dl, and the average glucose is 150 mg/dl,
+"""
+Still working on implementation
+def gmi(values):
+    mean_val = mean_glucose(values)
+    GMI( %) = 3.31 + 0.02392 x[mean glucose in mg / dL]
+    GMI(mmol / mol) = 12.71 + 4.70587 x[mean glucose in mmol / L]
+    #For example, if the SD is 50 mg/dl, and the average glucose is 150 mg/dl,
 # then you divide 50 by 150, multiply by 100, and you get a CV of 33%.
+"""
 
 
-def avg_glucose(values):
+
+def mean_glucose(bg_values):
+    """
+        Calculate the mean within a set of glucose values
+
+        Arguments:
+        values -- numpy array contains a list of bg values.
+
+        Output: Calculated Mean
+    """
+    return round(np.mean(bg_values), 2)
+
+def avg_glucose(bg_values):
     """
         Calculate the average within a set of glucose values
 
@@ -27,9 +47,9 @@ def avg_glucose(values):
 
         Output: Calculated Average
     """
-    return round(np.average(values), 2)
+    return round(np.average(bg_values), 2)
 
-def std_deviation(values):
+def std_deviation(bg_values):
     """
             Calculate the standard deviation within a set of glucose values
 
@@ -38,15 +58,15 @@ def std_deviation(values):
 
             Output: Calculated standard deviation
         """
-    return round(np.std(values), 2)
+    return round(np.std(bg_values), 2)
 
-def percent_values_by_range(values, lower_threshold: int, upper_threshold: int):
+def percent_values_by_range(bg_values, lower_threshold: int, upper_threshold: int):
     """
         Calculate the percent of values that match has a bg within the lower and upper threshold.
         The lower and upper values will be included in the range to calculate on.
 
         Arguments:
-        values -- numpy array contains a list of bg values.
+        bg_values -- numpy array contains a list of bg values.
         lower_threshold -- The the lower value in the range to calculate on.
         upper_threshold -- The the upper value in the range to calculate on.
 
@@ -54,81 +74,38 @@ def percent_values_by_range(values, lower_threshold: int, upper_threshold: int):
         Percent value
     """
     calc_low_thresh, calc_upper_thresh = _validate_input(lower_threshold, upper_threshold)
-    results = round(np.where((values <= calc_upper_thresh) & (values >= calc_low_thresh), 1, 0).sum()/values.size*100, 2)
+    results = round(np.where((bg_values <= calc_upper_thresh) & (bg_values >= calc_low_thresh), 1, 0).sum()/bg_values.size*100, 2)
     return results
 
 
-def percent_time_in_range(values, lower_threshold: int, upper_threshold: int):
+def percent_time_in_range(bg_values, lower_threshold: int, upper_threshold: int, time_delta=5):
     """
         Calculate the number of minutes the bg was within the lower and upper range.
         The lower and upper values will be included in the range to calculate on.
 
         Arguments:
-        values -- Panda Dataframe with a column named values that contains a list of bg values and a column named date
+        bg_values -- Panda Dataframe with a column named values that contains a list of bg values and a column named date
         containing a python datetime value.
         lower_threshold -- The the lower value in the range to calculate on.
         upper_threshold -- The the upper value in the range to calculate on.
 
         Output:
-        Percent value
+        Percent of bg values in range
     """
     calc_low_thresh, calc_upper_thresh = _validate_input(lower_threshold, upper_threshold)
-    values_sorted = values.sort_values(by='date').reindex()
-    date_df = values_sorted['date']
-
-    terst_dif = date_df.diff()
-    print("stop")
-
-    #1. get diff between previous record
-    #2. if in range set number.
-    #pd.to_datetime(values['date'], format='%m/%d/%Y %H:%M:%S')
-
-    from_date = None
-    date = None
-    match_length = 0
-    mins_in_range = 0
-    values_sorted = values.sort_values(by='date').reindex()
-    values_sorted.reset_index(drop=True, inplace=True)
-    set_length = values_sorted.shape[0]
-    hit = False
-    for i in values_sorted.index:
-        value = values_sorted.loc[i, 'values']
-        date = values_sorted.loc[i, 'date']
-        if   value <= calc_upper_thresh and value >= calc_low_thresh:
-            match_length += 1
-            hit = True
-            if from_date is None or date < from_date:
-                from_date = date
-        else:
-            if hit:
-                 sec = date - from_date
-                 mins_in_range += sec.seconds / 60
-
-            from_date = None
-            hit = False
-    if hit:
-        sec = date - from_date
-        mins_in_range += sec.seconds / 60
- 
-    return mins_in_range 
+    bg_df = bg_values['bg_values']
+    bg_np = bg_df.to_numpy()
+    in_range = np.count_nonzero((bg_np > calc_low_thresh) & (bg_np < calc_upper_thresh))
+    val_count = bg_df.count()
+    return round(in_range/val_count*100, 2)
  
 
-     
-
-def _validate_input(lower_threshold: int, upper_threshold: int):
-    calc_low_thresh = 0
-    if lower_threshold < 0 or upper_threshold < 0:
+def _validate_input(lower_threshold: int, upper_threshold: int) -> Tuple[int, int]:
+    if any(num < 0 for num in [lower_threshold, upper_threshold]):
         raise Exception("lower and upper thresholds must be a non-negative number")
-
-    if lower_threshold and lower_threshold is not 0:
-        calc_low_thresh = lower_threshold
-
-    calc_upper_thresh = 1000
-    if upper_threshold and upper_threshold is not 0:
-        calc_upper_thresh = upper_threshold
-
-    if calc_low_thresh > calc_upper_thresh:
+    if upper_threshold == 0:
+        upper_threshold = 1000
+    if lower_threshold > upper_threshold:
         raise Exception("lower threshold is higher than the upper threshold.")
-
-    return calc_low_thresh, calc_upper_thresh
+    return lower_threshold, upper_threshold
 
--- metrics/tests/test_metrics.py
+++ metrics/tests/test_metrics.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import pytest
-import numpy as np
 from metrics.metrics import percent_values_by_range, percent_time_in_range, \
     avg_glucose, std_deviation, cv_of_glucose
 import datetime
 
-""" 
+
 def test_calculation():
     pd_values = get_values()
     percent = percent_values_by_range(pd_values.to_numpy(), 100, 0)
@@ -26,45 +25,27 @@ def test_missing_lower_number():
      with pytest.raises(TypeError) as excinfo:
         percent = percent_values_by_range(get_values(), 100)
 
-
-def test_lower_number_highjer_than_upper_number():
+def test_lower_number_higher_than_upper_number():
     with pytest.raises(Exception) as excinfo:
         percent = percent_values_by_range(get_values(), 100, 20)
+    print(str(excinfo.value))
     assert "lower threshold is higher than the upper threshold." in str(excinfo.value)
 
 
 def test_percent_time_in_range():
     #df = (df.assign(delta=df.sort_values(['ts']).groupby(['url', 'service']).diff(1)))
     percent = percent_time_in_range(get_date_values(), 100, 150)
-    assert  percent == 40
+    assert  percent == 47.06
 
-
-def test_percent():
-    low_thresh = pd.date_range('2015-02-23', periods=5, freq='T')
-    value = pd.date_range('2015-02-24', periods=5, freq='T')
-    upper_thresh = pd.date_range('2015-02-25', periods=5, freq='T')
-    df = pd.DataFrame({'low_thresh': low_thresh, 'upper_thresh': upper_thresh, 'value': value})
-    print(df)
-    print(df.info())
-    df['difference'] = 'nan'
-    df['difference'][(df['value'] > df['low_thresh']) & (df['upper_thresh'] > df['value'])] = df['upper_thresh'] - \
-                                                                                              df['low_thresh']
-    print(df)
-    
-    
 def test_avg_glucose():
     pd_values = get_values()
     average = avg_glucose(pd_values.to_numpy())
     assert  average == 86.48 
 
-
 def test_std_deviation():
     pd_values = get_values()
     std = std_deviation(pd_values.to_numpy())
     assert  std == 11.90
-    
-
-"""
 
 def test_cv_of_glucose():
     pd_values = get_values()
@@ -174,7 +155,7 @@ def get_values():
     [60],
     [50]]
 
-    return pd.DataFrame(values, columns=['values'])
+    return pd.DataFrame(values, columns=['bg_values'])
 
 def get_date_values():
     values = [['8/15/2019 00:40:00',87],
@@ -201,4 +182,4 @@ def get_date_values():
         new_array.append([date, i[1]])
 
 
-    return pd.DataFrame(new_array, columns=['date','values'])
+    return pd.DataFrame(new_array, columns=['date','bg_values'])
